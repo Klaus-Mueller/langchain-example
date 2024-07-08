ce0c63b8bf734c92589d5bac493a7020dff8bab4
Klaus Mueller <Klausmueller@outlook.com>
2024-07-08 19:03:47 -0300

    Leverage from Neo4J to save the chat history

--- src/dark-souls-chat.py
+++ src/dark-souls-chat.py
@@ -4,12 +4,11 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain.schema import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder 
 from langchain_community.chat_message_histories import Neo4jChatMessageHistory
-from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_community.graphs import Neo4jGraph
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from uuid import uuid4
 
-
-memory = ChatMessageHistory()
-
+SESSION_ID = str(uuid4())
 def get_openAPI_llm():
     # Retrieve the API key from the environment variable
     api_key = os.getenv("OPENAI_API_KEY")
@@ -24,18 +23,18 @@ def get_openAPI_llm():
 
     return chat_llm
 
-def get_chat_history():
-    history = Neo4jChatMessageHistory(
+def getNeo4jConnection():
+    db = Neo4jGraph(
         url="bolt://localhost:7687",
         username="neo4j",
         password="ds123123",
-        session_id="session_id_1",
     )
-    return history
+    return db
 
 
 def get_memory(session_id):
-    return memory
+    return Neo4jChatMessageHistory(session_id=session_id, graph=getNeo4jConnection())
+
 
 
 if __name__ == "__main__":
@@ -73,8 +72,6 @@ if __name__ == "__main__":
         ]
     }
     """
-    session_id = "unique_session_id_1"
-
     while True:
         question = input("> ")
         response = chat_with_message_history.invoke(
@@ -82,7 +79,7 @@ if __name__ == "__main__":
                 "context" : defeated_bosses,
                 "question": question
             },
-            config={"configurable": {"session_id": session_id}}
+            config={"configurable": {"session_id": SESSION_ID}}
         )
         
         print(response)
